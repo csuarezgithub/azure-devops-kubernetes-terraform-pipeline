3b979d21377305d0fc2b4098f4b5291387372e04
Cristian Suarez <crisuarezm@bancoripley.com>
2022-01-13 09:02:23 -0300

    add version providers kubernetes v1

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -26,8 +26,8 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "1.13.4"
+  #load_config_file       = false
+  #version                = "1.13.4"
 }
 
 module "in28minutes-cluster" {
